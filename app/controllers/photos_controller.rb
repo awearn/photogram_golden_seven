c69f7c64e782cf735d00193c2f87506503fcdba7
awearn <awearn2016@kellogg.northwestern.edu>
2015-05-10 17:37:31 -0500

    Update 1

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,5 +3,22 @@ class PhotosController < ApplicationController
     @list_of_photos = Photo.all
   end
 
+  def show
+    @id = params["id"]
+    @photo = Photo.find_by({ :id=> params["id"]})
+  end
 
+  def new_form
+
+  end
+
+  def create_row
+    @url = params["the_source"]
+    @caption = params["the_caption"]
+    z = Photo.new
+    z.caption = @caption
+    z.source = @url
+    z.save
+    redirect_to("http://localhost:3000/photos")
+  end
 end
