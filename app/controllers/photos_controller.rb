72463c9a336b51799eaaab756b7edb573c290022
awearn <awearn2016@kellogg.northwestern.edu>
2015-05-10 19:15:02 -0500

    Update 2

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -5,7 +5,7 @@ class PhotosController < ApplicationController
 
   def show
     @id = params["id"]
-    @photo = Photo.find_by({ :id=> params["id"]})
+    @photo = Photo.find(params["id"])
   end
 
   def new_form
@@ -16,6 +16,27 @@ class PhotosController < ApplicationController
     @url = params["the_source"]
     @caption = params["the_caption"]
     z = Photo.new
+    z.caption = @caption
+    z.source = @url
+    z.save
+    redirect_to("http://localhost:3000/photos")
+  end
+
+  def destroy
+    @photo = Photo.find(params["id"])
+    @photo.destroy
+    redirect_to("http://localhost:3000/photos")
+  end
+
+  def edit_form
+    @id = params["id"]
+    @photo = Photo.find(params["id"])
+  end
+
+  def update_row
+    @url = params["the_source"]
+    @caption = params["the_caption"]
+    z = Photo.find(params["id"])
     z.caption = @caption
     z.source = @url
     z.save
